2d960f3e27fe1c0ba7a301bc542f7218d60c2163
fanyuqing <>
2023-06-13 17:36:32 +0800

    feat:加入滑动滚动

--- lib/widget_study_modules/scrolls/nested_scroll/nested_scroll_view.dart
+++ lib/widget_study_modules/scrolls/nested_scroll/nested_scroll_view.dart
@@ -35,7 +35,10 @@ class _NestedScrollPageState extends State<NestedScrollPage> with SingleTickerPr
   Widget build(BuildContext context) {
     statusBarHeight = MediaQuery.of(context).padding.top;
     pinnedHeaderHeight = statusBarHeight! + kToolbarHeight;
+
+    /// (3) pinnedHeaderHeight的值就是从顶部加上bar的高度
     return Scaffold(
+      /// (1)选用ExtendedNestedScrollView
       body: ExtendedNestedScrollView(
         headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
           return [
@@ -61,6 +64,8 @@ class _NestedScrollPageState extends State<NestedScrollPage> with SingleTickerPr
             ),
           ];
         },
+
+        /// (2) 调用pinnedHeaderSliverHeightBuilder 返回maxScrollExtent最大扩展宽度
         pinnedHeaderSliverHeightBuilder: () {
           return pinnedHeaderHeight!;
         },
